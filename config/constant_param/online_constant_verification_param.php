86da38b2532a778529b165b805e4a171544d6166
SOLANKE Abiodun <solanke.abdullahi@gmail.com>
2022-05-03 20:06:47 +0200

    Update online_constant_verification_param.php

--- config/constant_param/online_constant_verification_param.php
+++ config/constant_param/online_constant_verification_param.php
@@ -7,10 +7,6 @@ define("GATEWAYURL", "http://www.remitademo.net/remita/ecomm/init.reg");//demo
 define("PATH", 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
 */
 
-
-//define("MERCHANTID", "573566089");
-//define("SERVICETYPEID", "574141886");
-//define("APIKEY", "695470");
 define("CHECKSTATUSURL", "https://login.remita.net/remita/ecomm/merchantId/OrderId/hash/orderstatus.reg");
 define("GATEWAYURL", "https://login.remita.net/remita/ecomm/init.reg");
 define("PATH", 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
