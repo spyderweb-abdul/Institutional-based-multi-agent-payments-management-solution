9b04538fd3a79a7dbb06757352eed88b777a90ef
SOLANKE Abiodun <solanke.abdullahi@gmail.com>
2022-05-03 20:06:16 +0200

    Update constant_verification_param.php

--- config/constant_param/constant_verification_param.php
+++ config/constant_param/constant_verification_param.php
@@ -1,15 +1,15 @@
 <?php
-/*define("MERCHANTID", "2547916");
-define("SERVICETYPEID", "4430731");
-define("APIKEY", "1946");
+/*define("MERCHANTID", "000000");
+define("SERVICETYPEID", "000000");
+define("APIKEY", "0000");
 define("CHECKSTATUSURL", "http://www.remitademo.net/remita/ecomm");
 define("GATEWAYURL", "http://www.remitademo.net/remita/ecomm/init.reg");
 */
 
 
-//define("MERCHANTID", "573566089");
-//define("SERVICETYPEID", "574141886");
-//define("APIKEY", "695470");
+//define("MERCHANTID", "000000000");
+//define("SERVICETYPEID", "0000000");
+//define("APIKEY", "0000");
 define("CHECKSTATUSURL", "https://login.remita.net/remita/ecomm");
 define("GATEWAYURL", "https://login.remita.net/remita/ecomm/init.reg");
 define("PATH", 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
