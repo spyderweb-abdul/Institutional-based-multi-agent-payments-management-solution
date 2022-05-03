8a194be355ad569699be6b469128575e7df36e56
SOLANKE Abiodun <solanke.abdullahi@gmail.com>
2022-05-03 20:18:57 +0200

    Delete adminPanel directory

--- adminPanel/logoutAdminNow.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-   $msg = "User ".$_SESSION['userID']." has logged out";
-   unset($_SESSION['userID']);
-   session_destroy();
-   header("Location: ../index.php?msg=$msg");
-
-?>
--- adminPanel/userSessionLog.php
+++
@@ -1,9 +0,0 @@
-<?php
-			
- echo '<div class="btn-group btn-group-sm btn">';
- echo '<button class="btn btn-default"> You are logged on as: '.$_SESSION['userID'].'</button>';
- echo '<a href="logoutAdminNow.php" class="btn btn-default"> Logout </a>'; 
-// echo '<button class="btn btn-success btn-sm styleTb2"> Active </button>';
- echo '</div>';
-
-?>
