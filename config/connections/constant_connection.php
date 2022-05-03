2ba4314dbc15433cd1eb78f2a2de3437ba7eb6fb
SOLANKE Abiodun <solanke.abdullahi@gmail.com>
2022-05-03 20:01:52 +0200

    Update constant_connection.php

--- config/connections/constant_connection.php
+++ config/connections/constant_connection.php
@@ -1,8 +1,8 @@
 <?php
-define("PAYMENT_DB", "udus_payment_db");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_HOST", "localhost");
+define("PAYMENT_DB", "db_name");
+define("DB_USER", "db_user_name");
+define("DB_PASS", "db_paswword");
+define("DB_HOST", "db_host");
 
 $paydb = new MySQLi(DB_HOST, DB_USER, DB_PASS, PAYMENT_DB);
 if ($paydb->connect_error) { die('Connection Error (' . $paydb->connect_errno . ') ' . $paydb->connect_error); }
